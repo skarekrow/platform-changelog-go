1c3dda5741f30d2201a6ce82c6ec2de17944ea75
Stephen Adams <stephen.adams@redhat.com>
2022-04-20 16:33:37 -0400

    [fix] fix a typo in commits.go
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- internal/endpoints/commits.go
+++ internal/endpoints/commits.go
@@ -8,6 +8,7 @@ import (
 )
 
 func GetCommitsAll(w http.ResponseWriter, r *http.Request) {
+	incRequests(r.URL.Path, r.Method, r.UserAgent())
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(db.GetCommitsAll(db.DB))
