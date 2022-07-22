39c58d1eb110e12cebc38f5086b351235bbf05e6
Justin Orringer <justinorringer@gmail.com>
2022-07-22 16:57:29 -0400

    /services endpoint returns latest commit AND deploy again

--- internal/db/services.go
+++ internal/db/services.go
@@ -16,25 +16,31 @@ func CreateServiceTableEntry(db *gorm.DB, name string, s config.Service) (result
 	return results, newService
 }
 
-func GetServicesAll(db *gorm.DB) (*gorm.DB, []structs.ServicesData) {
+func GetServicesAll(db *gorm.DB) (*gorm.DB, []structs.ExpandedServicesData) {
 	callDurationTimer := prometheus.NewTimer(metrics.SqlGetServicesAll)
 	defer callDurationTimer.ObserveDuration()
-	var services []structs.ServicesData
+	var services []structs.ExpandedServicesData
 	result := db.Model(models.Services{}).Find(&services)
 
-	var servicesWithTimelines []structs.ServicesData
+	var servicesWithTimelines []structs.ExpandedServicesData
 	for i := 0; i < len(services); i++ {
-		_, s := GetLatest(db, services[i])
+		_, _, s := GetLatest(db, services[i])
+
 		servicesWithTimelines = append(servicesWithTimelines, s)
 	}
 
 	return result, servicesWithTimelines
 }
 
-func GetLatest(db *gorm.DB, service structs.ServicesData) (*gorm.DB, structs.ServicesData) {
+func GetLatest(db *gorm.DB, service structs.ExpandedServicesData) (*gorm.DB, *gorm.DB, structs.ExpandedServicesData) {
 	l.Log.Debugf("Query name: %s", service.Name)
-	result := db.Model(models.Timelines{}).Select("*").Joins("JOIN services ON timelines.service_id = services.id").Where("services.name = ?", service.Name).Where("timelines.type = ?", "commit").Order("Timestamp desc").Limit(1).Find(&service.Timelines)
-	return result, service
+
+	// TODO: Make one query to get the latest commit and deploy for each service
+	comResult := db.Model(models.Timelines{}).Select("*").Joins("JOIN services ON timelines.service_id = services.id").Where("services.name = ?", service.Name).Where("timelines.type = ?", "commit").Order("Timestamp desc").Limit(1).Find(&service.Commit)
+
+	depResult := db.Model(models.Timelines{}).Select("*").Joins("JOIN services ON timelines.service_id = services.id").Where("services.name = ?", service.Name).Where("timelines.type = ?", "deploy").Order("Timestamp desc").Limit(1).Find(&service.Deploy)
+
+	return comResult, depResult, service
 }
 
 func GetServiceByName(db *gorm.DB, name string) (*gorm.DB, structs.ServicesData) {
--- internal/structs/structs.go
+++ internal/structs/structs.go
@@ -1,20 +1,32 @@
 package structs
 
 type ServicesData struct {
-	ID          int             `json:"id,omitempty"`
-	Name        string          `json:"name,omitempty"`
-	DisplayName string          `json:"display_name,omitempty"`
-	GHRepo      string          `json:"gh_repo,omitempty"`
-	GLRepo      string          `json:"gl_repo,omitempty"`
-	DeployFile  string          `json:"deploy_file,omitempty"`
-	Namespace   string          `json:"namespace,omitempty"`
-	Branch      string          `json:"branch,omitempty"`
-	Timelines   []TimelinesData `json:"commits,omitempty" gorm:"foreignkey:ID"`
+	ID          int    `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	DisplayName string `json:"display_name,omitempty"`
+	GHRepo      string `json:"gh_repo,omitempty"`
+	GLRepo      string `json:"gl_repo,omitempty"`
+	DeployFile  string `json:"deploy_file,omitempty"`
+	Namespace   string `json:"namespace,omitempty"`
+	Branch      string `json:"branch,omitempty"`
+}
+
+type ExpandedServicesData struct {
+	ID          int           `json:"id,omitempty"`
+	Name        string        `json:"name,omitempty"`
+	DisplayName string        `json:"display_name,omitempty"`
+	GHRepo      string        `json:"gh_repo,omitempty"`
+	GLRepo      string        `json:"gl_repo,omitempty"`
+	DeployFile  string        `json:"deploy_file,omitempty"`
+	Namespace   string        `json:"namespace,omitempty"`
+	Branch      string        `json:"branch,omitempty"`
+	Commit      TimelinesData `json:"latest_commit,omitempty" gorm:"foreignkey:ID"`
+	Deploy      TimelinesData `json:"latest_deploy,omitempty" gorm:"foreignkey:ID"`
 }
 
 type TimelinesData struct {
 	ID              int    `json:"id" gorm:"primary_key"`
-	ServiceID       int    `json:"service_id" gorm:"not null;foreign_key:services_data.id"`
+	ServiceID       int    `json:"service_id" gorm:"foreign_key:services_data.id"`
 	Type            string `json:"type"`
 	Timestamp       string `json:"timestamp"`
 	Repo            string `json:"repo"`
