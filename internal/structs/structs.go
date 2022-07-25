9ee2e2453b8e3ce6885b80d2a18937f9fdc2f1cc
Justin Orringer <justinorringer@gmail.com>
2022-07-25 13:17:19 -0400

    Removed unnecessary ommitempty fields

--- internal/structs/structs.go
+++ internal/structs/structs.go
@@ -1,36 +1,36 @@
 package structs
 
 type ServicesData struct {
-	ID          int    `json:"id,omitempty"`
-	Name        string `json:"name,omitempty"`
-	DisplayName string `json:"display_name,omitempty"`
-	GHRepo      string `json:"gh_repo,omitempty"`
-	GLRepo      string `json:"gl_repo,omitempty"`
-	DeployFile  string `json:"deploy_file,omitempty"`
-	Namespace   string `json:"namespace,omitempty"`
-	Branch      string `json:"branch,omitempty"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	DisplayName string `json:"display_name"`
+	GHRepo      string `json:"gh_repo"`
+	GLRepo      string `json:"gl_repo"`
+	DeployFile  string `json:"deploy_file"`
+	Namespace   string `json:"namespace"`
+	Branch      string `json:"branch"`
 }
 
 type ExpandedServicesData struct {
-	ID          int           `json:"id,omitempty"`
-	Name        string        `json:"name,omitempty"`
-	DisplayName string        `json:"display_name,omitempty"`
-	GHRepo      string        `json:"gh_repo,omitempty"`
-	GLRepo      string        `json:"gl_repo,omitempty"`
-	DeployFile  string        `json:"deploy_file,omitempty"`
-	Namespace   string        `json:"namespace,omitempty"`
-	Branch      string        `json:"branch,omitempty"`
-	Commit      TimelinesData `json:"latest_commit,omitempty" gorm:"foreignkey:ID"`
-	Deploy      TimelinesData `json:"latest_deploy,omitempty" gorm:"foreignkey:ID"`
+	ID          int           `json:"id"`
+	Name        string        `json:"name"`
+	DisplayName string        `json:"display_name"`
+	GHRepo      string        `json:"gh_repo"`
+	GLRepo      string        `json:"gl_repo"`
+	DeployFile  string        `json:"deploy_file"`
+	Namespace   string        `json:"namespace"`
+	Branch      string        `json:"branch"`
+	Commit      TimelinesData `json:"latest_commit" gorm:"foreignkey:ID"`
+	Deploy      TimelinesData `json:"latest_deploy" gorm:"foreignkey:ID"`
 }
 
 type TimelinesData struct {
 	ID              int    `json:"id" gorm:"primary_key"`
 	ServiceID       int    `json:"service_id" gorm:"foreign_key:services_data.id"`
 	Type            string `json:"type"`
-	Timestamp       string `json:"timestamp"`
 	Repo            string `json:"repo"`
 	Ref             string `json:"ref"`
+	Timestamp       string `json:"timestamp"`
 	Author          string `json:"author,omitempty"`
 	MergedBy        string `json:"merged_by,omitempty"`
 	Message         string `json:"message,omitempty"`
